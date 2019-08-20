e43618ec6dfb2b93c7359cbaecdf398fc7175da2
tviti <tviti@hawaii.edu>
2019-08-19 20:03:17 -1000

    Added lualatex to tex-compile-commands

--- config/latex-mode_config.el
+++ config/latex-mode_config.el
@@ -9,6 +9,14 @@ a shift-cmd-click from within Skim to jump to the associated line in emacs"
   (server-start)  ; Should we check if a daemon is already running?
   (add-hook 'server-switch-hook 'raise-emacs-on-aqua))
 
-;; Might be worth-while expanding this to run on *nix' too. Does Skim run on Centos?
+;; Might be worth-while expanding this to run on *nix' too. Does Skim run on
+;; Centos? Do we also want this to only run when tex-mode activated?
 (if (string-equal system-type "darwin")
     (skim-config))
+
+;; Use lualatex for compilation, since this has much bettter unicode support
+;; than pdflatex
+(add-hook 'tex-mode-hook
+	  (lambda ()
+	    (add-to-list 'tex-compile-commands
+			 '("lualatex %r.tex" nil "%r.pdf") t)))
