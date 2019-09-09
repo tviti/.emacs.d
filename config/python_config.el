2e606a0fa9e7c1558afdf6a6dea97e2e099c6d0b
Taylor Viti <tviti@hawaii.edu>
2019-09-09 13:27:08 -1000

    Allow the elpy to "see" conda envs on my laptop

--- config/python_config.el
+++ config/python_config.el
@@ -5,10 +5,14 @@
 
 ;; Use jupyter console, or on os x use ipython with workaround for https://github.com/jorgenschaefer/elpy/issues/1550
 (if (string-equal system-type "darwin")
-    (setq python-shell-interpreter "ipython"
+    (setq python-shell-interpreter "/usr/bin/env ipython"
 	  python-shell-interpreter-args "--simple-prompt -c exec('__import__(\\'readline\\')') -i"))
   ;; (setq python-shell-interpreter "jupyter"
   ;; 	python-shell-interpreter-args "console --simple-prompt"
   ;; 	python-shell-prompt-detect-failure-warning nil)
   ;; (add-to-list 'python-shell-completion-native-disabled-interpreters
-  ;; 	       "jupyter"))
+;; 	       "jupyter"))
+
+;; in "Virtual Envs" menu, show me conda envs
+(if (string-equal system-name "R-Daneel.local")
+    (setenv "WORKON_HOME" "~/anaconda2/envs"))
