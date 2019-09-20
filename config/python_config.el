333dcb90f886f0f00cc323242dae86db139a5aec
Taylor Viti <tviti@hawaii.edu>
2019-09-20 11:55:21 -1000

    Using ipython on macOS and jupyter on others for repl.

--- config/python_config.el
+++ config/python_config.el
@@ -1,18 +1,19 @@
 (elpy-enable)
 
-;; Automatically use the BIG virtualenv (make sure to unset this when we are done with this project!)
-;; (pyvenv-activate "~/anaconda2/envs/BIG")
+;; Force elpy to use python3 for backend language features
+(setq elpy-rpc-python-command "python3")
 
 ;; Use jupyter console, or on os x use ipython with workaround for https://github.com/jorgenschaefer/elpy/issues/1550
 (if (string-equal system-type "darwin")
-    (setq python-shell-interpreter "/usr/bin/env ipython"
-	  python-shell-interpreter-args "--simple-prompt -c exec('__import__(\\'readline\\')') -i"))
-  ;; (setq python-shell-interpreter "jupyter"
-  ;; 	python-shell-interpreter-args "console --simple-prompt"
-  ;; 	python-shell-prompt-detect-failure-warning nil)
-  ;; (add-to-list 'python-shell-completion-native-disabled-interpreters
-;; 	       "jupyter"))
+    (setq python-shell-interpreter "ipython"
+	  python-shell-interpreter-args "--simple-prompt -c exec('__import__(\\'readline\\')') -i")
+  (setq python-shell-interpreter "jupyter"
+  	python-shell-interpreter-args "console --simple-prompt"
+  	python-shell-prompt-detect-failure-warning nil)
+  (add-to-list 'python-shell-completion-native-disabled-interpreters
+  	       "jupyter"))
+
 
 ;; in "Virtual Envs" menu, show me conda envs
-(if (string-equal system-name "R-Daneel.local")
-    (setenv "WORKON_HOME" "~/anaconda2/envs"))
+;; (if (string-equal system-name "R-Daneel.local")
+;;     (setenv "WORKON_HOME" "~/anaconda2/envs"))
