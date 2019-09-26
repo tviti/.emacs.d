43cd2d2235378ddc65d01439c66d50c09e2937fc
tviti <tviti@hawaii.edu>
2019-09-25 16:12:19 -1000

    Using polymode + poly-markdown + poly-R for editing .Rmd docs.

--- config/ESS_config.el
+++ config/ESS_config.el
@@ -2,3 +2,8 @@
 ;; Rstudio style rules
 (add-hook 'ess-mode-hook
 	  (lambda () (ess-set-style 'RStudio)))
+
+;; Use polymode for working with .Rmd files
+(require 'polymode)
+(require 'poly-markdown)
+(require 'poly-R)
