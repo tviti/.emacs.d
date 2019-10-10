7053b694a11c63957dd807387df920a9fb2d7cba
Taylor Viti <tviti@hawaii.edu>
2019-10-10 03:10:54 -1000

    Removed `raise-emacs-on-aqua' because no longer necessary.

--- config/latex-mode_config.el
+++ config/latex-mode_config.el
@@ -9,16 +9,11 @@
 (add-hook 'LaTeX-mode-hook
 	  (lambda () (flycheck-mode 1)))
 
-(defun raise-emacs-on-aqua ()
-  "Auto-raise emacs on activation"
-  (shell-command "osascript -e 'tell application \"Emacs\" to activate' &"))
-
 (defun skim-config ()
   "Setup integration w/ Skim pdf viewer, per \
 https://sourceforge.net/p/skim-app/wiki/TeX_and_PDF_Synchronization/#setting-up-emacs. Do \
 a shift-cmd-click from within Skim to jump to the associated line in emacs"
-  (server-start)  ; Should we check if a daemon is already running?
-  (add-hook 'server-switch-hook 'raise-emacs-on-aqua))
+  (server-start))  ; Should we check if a daemon is already running?
 
 ;; Might be worth-while expanding this to run on *nix' too. Does Skim run on
 ;; Centos? Do we also want this to only run when tex-mode activated?
