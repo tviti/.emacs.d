2df88e1b0986ab9a862049ee3591391994d66ca5
Taylor Viti <tviti@hawaii.edu>
2019-10-10 03:12:05 -1000

    Removed workaround for fixed GnuTLS bug.

--- config/osx_config.el
+++ config/osx_config.el
@@ -6,8 +6,8 @@
 ;; Fix for "unknown/untrusted cert signing authority" error from
 ;; package manager, per
 ;; https://blog.vifortech.com/posts/emacs-tls-fix/
-(require 'gnutls)
-(add-to-list 'gnutls-trustfiles "/usr/local/etc/openssl/cert.pem")
+;; (require 'gnutls)
+;; (add-to-list 'gnutls-trustfiles "/usr/local/etc/openssl/cert.pem")
 
 ;; Make the font a little bit bigger for my laptop
 (set-face-attribute 'default nil :height 180)
