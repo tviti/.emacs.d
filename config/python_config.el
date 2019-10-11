714efc243cb2f1c9a8fb83298629bfb72fde2860
tviti <tviti@hawaii.edu>
2019-10-10 16:07:43 -1000

    Added defun to launch jupyter-notebook server.

--- config/python_config.el
+++ config/python_config.el
@@ -21,3 +21,10 @@
 ;; in "Virtual Envs" menu, show me conda envs
 ;; (if (string-equal system-name "R-Daneel.local")
 ;;     (setenv "WORKON_HOME" "~/anaconda2/envs"))
+
+;; Launch a Jupyter Notebook server (uses the active pyvenv)
+(defun jupyter-nb-server-launch ()
+  (interactive)
+  (let ((buffer "*Jupyter Notebook Server*"))
+    (async-shell-command  "jupyter-notebook" buffer buffer)))
+			
