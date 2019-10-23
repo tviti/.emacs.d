c43ce00a6dac53b9da01d99c9f274febf40ec383
Taylor Viti <tviti@hawaii.edu>
2019-10-23 11:05:24 -1000

    Enable ivy completions in SLIME.

--- config/completion_config.el
+++ config/completion_config.el
@@ -12,6 +12,17 @@
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
 
+;; SLIME won't actually use the ivy backend ootb. Per abo-abo: "Looks like SLIME
+;; is really old school, and doesn't obey the completion-in-region-function
+;; API. We can make it work with advice":
+(require 'slime)
+(defun ora-slime-completion-in-region (_fn completions start end)
+  (funcall completion-in-region-function start end completions))
+
+(advice-add
+ 'slime-display-or-scroll-completions
+ :around #'ora-slime-completion-in-region)
+
 ;; Keybindings
 (require 'org)
 (define-key org-mode-map (kbd "C-c C-j") nil)
