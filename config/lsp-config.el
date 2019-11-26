6c0387129d8d1d48954f25829505f9eb92a6e153
Taylor Viti <tviti@hawaii.edu>
2019-11-26 11:24:49 -1000

    config: Temporarily commented out julia and lsp configs.
    These are currently unused, so I have commented them out to simplify
    integrating nix + emacs.

--- config/lsp-config.el
+++ config/lsp-config.el
@@ -1,34 +1,34 @@
-(require 'julia-mode)
-(require 'lsp-julia)
-(require 'lsp-mode)
-
-(require 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
-
-;; Configure lsp + julia
-(add-hook 'julia-mode-hook #'lsp-mode)
-(add-hook 'julia-mode-hook #'lsp)
-
-;; Don't bomb the user with object documentation
-(setq lsp-ui-doc-enable 'nil)
-(setq lsp-ui-sideline-enable 'nil)
-
-;; Keybindings
-(define-key lsp-ui-mode-map (kbd "C-c l") 'lsp-ui-imenu)
-
-;; Override xref keybindings w/ lsp-ui-peek
-(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-
-;; (define-key lsp-ui-mode-map (kbd "\C-c d") #'lsp-ui-doc-show)
-;; (define-key lsp-ui-mode-map (kbd "\C-u \C-c d") #'lsp-ui-doc-hide)
-
-(setq lsp-eldoc-enable-hover 't)
-(setq lsp-eldoc-render-all 'nil)
-(setq lsp-eldoc-prefer-signature-help 't)
-
-;; (setq lsp-log-io 't)
-(setq lsp-log-io 'ninill)
-;; (setq lsp-signature-render-all 'nil)
-
+;; (require 'julia-mode)
+;; (require 'lsp-julia)
+;; (require 'lsp-mode)
+;; 
+;; (require 'lsp-ui)
+;; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+;; 
+;; ;; Configure lsp + julia
+;; (add-hook 'julia-mode-hook #'lsp-mode)
+;; (add-hook 'julia-mode-hook #'lsp)
+;; 
+;; ;; Don't bomb the user with object documentation
+;; (setq lsp-ui-doc-enable 'nil)
+;; (setq lsp-ui-sideline-enable 'nil)
+;; 
+;; ;; Keybindings
+;; (define-key lsp-ui-mode-map (kbd "C-c l") 'lsp-ui-imenu)
+;; 
+;; ;; Override xref keybindings w/ lsp-ui-peek
+;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+;; 
+;; ;; (define-key lsp-ui-mode-map (kbd "\C-c d") #'lsp-ui-doc-show)
+;; ;; (define-key lsp-ui-mode-map (kbd "\C-u \C-c d") #'lsp-ui-doc-hide)
+;; 
+;; (setq lsp-eldoc-enable-hover 't)
+;; (setq lsp-eldoc-render-all 'nil)
+;; (setq lsp-eldoc-prefer-signature-help 't)
+;; 
+;; ;; (setq lsp-log-io 't)
+;; (setq lsp-log-io 'ninill)
+;; ;; (setq lsp-signature-render-all 'nil)
+;; 
 (provide 'lsp-config)
