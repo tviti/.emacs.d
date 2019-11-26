b42100acbffca3d8822310a8c77e8cda09c0c9f2
Taylor Viti <tviti@hawaii.edu>
2019-11-26 11:27:14 -1000

    matlab-config: Moved hook based configuration to a single defun.

--- config/matlab-config.el
+++ config/matlab-config.el
@@ -10,33 +10,50 @@
     (setq matlab-shell-command-switches '("-nodesktop" "-softwareopengl")))
 
 ;; shell mode keybindings
-(add-hook 'matlab-shell-mode-hook
-	  (lambda () (local-set-key (kbd "\C-c a") 'matlab-shell-close-figures)))
-(add-hook 'matlab-shell-mode-hook
-	  (lambda () (local-set-key (kbd "\C-c o") 'matlab-shell-close-current-figure)))
-(add-hook 'matlab-shell-mode-hook
-	  (lambda () (local-set-key (kbd "\C-c v") 'matlab-shell-clear-all)))
+;; (add-hook 'matlab-shell-mode-hook
+;; 	  (lambda () (local-set-key (kbd "\C-c a") 'matlab-shell-close-figures)))
+;; (add-hook 'matlab-shell-mode-hook
+;; 	  (lambda () (local-set-key (kbd "\C-c o") 'matlab-shell-close-current-figure)))
+;; (add-hook 'matlab-shell-mode-hook
+;; 	  (lambda () (local-set-key (kbd "\C-c v") 'matlab-shell-clear-all)))
 
 ;; editor mode keybindings
-(add-hook 'matlab-mode-hook
-	  (lambda () (local-set-key (kbd "\C-c a") 'matlab-shell-close-figures)))
-(add-hook 'matlab-mode-hook
-	  (lambda () (local-set-key (kbd "\C-c \C-c") 'matlab-shell-run-cell)))
-(add-hook 'matlab-mode-hook
-	  (lambda () (local-set-key (kbd "\C-c v") 'matlab-shell-clear-all)))
+;; (add-hook 'matlab-mode-hook
+;; 	  (lambda () (local-set-key (kbd "\C-c a") 'matlab-shell-close-figures)))
+;; (add-hook 'matlab-mode-hook
+;; 	  (lambda () (local-set-key (kbd "\C-c \C-c") 'matlab-shell-run-cell)))
+;; (add-hook 'matlab-mode-hook
+;; 	  (lambda () (local-set-key (kbd "\C-c v") 'matlab-shell-clear-all)))
 
 ;; change comment string to be compatible with matlab IDE
 (setq matlab-comment-region-s "% ")
 
 ;; Set the fill-column in emacs to be consistent with the default text limit
 ;; indicator location in the matlab gui editor
-(add-hook 'matlab-mode-hook
-	  (lambda () (setq-default fill-column 75)))
+;; (add-hook 'matlab-mode-hook
+;; 	  (lambda () (setq-default fill-column 75)))
 
-;; Line numbers don't work by default for some reason. Turn them on.
-(add-hook 'matlab-mode-hook
-	  (lambda ()
-	    (display-line-numbers-mode)
-	    (setq display-line-numbers 'relative)))
+;; (add-hook 'matlab-mode-hook
+;; 	  (lambda ()
+;; 	    (display-line-numbers-mode)
+;; 	    (setq display-line-numbers 'relative)))
+
+(defun tviti/matlab-mode-setup ()
+  "Apply configuration for `matlab-mode'."
+  (company-mode 1)
+  ;; Set the fill-column in emacs to be consistent with the default text limit
+  ;; indicator location in the matlab gui editor
+  (set-fill-column 75)
+  
+  ;; Line numbers don't work by default for some reason. Turn them on.
+  (display-line-numbers-mode)
+  (setq display-line-numers 'relative))
+
+(defun tviti/M-shell-mode-setup ()
+  "Apply configuration for `M-shell-mode'."
+  (company-mode 1))
+
+(add-hook 'matlab-mode-hook #'tviti/matlab-mode-setup)
+(add-hook 'M-shell-mode #'tviti/M-shell-mode-setup)
 
 (provide 'matlab-config)
