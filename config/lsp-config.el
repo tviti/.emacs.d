896c8adcfc982911a355f3e4cd1309a4610385d9
Taylor Viti <tviti@hawaii.edu>
2019-12-11 20:54:13 -1000

    lsp-config: Added command for build via texlab.

--- config/lsp-config.el
+++ config/lsp-config.el
@@ -8,6 +8,27 @@
 (add-to-list 'eglot-server-programs  '((tex-mode bibtex-mode latex-mode yatex-mode) "texlab"))
 
 (require 'linter-config)
+(require 'tex) ;; auctex
+
+(defun tviti/eglot-texlab-build ()
+  "Call the method textDocument/build on `TeX-master'."
+  (interactive)
+  (lexical-let ((fname (if (eq TeX-master t)
+			(buffer-file-name)
+			(file-truename (format "%s.tex" TeX-master))))
+		(docname (expand-file-name (TeX-active-master (TeX-output-extension)))))
+    (jsonrpc-async-request (eglot--current-server-or-lose) :textDocument/build
+			   `(:textDocument
+			     (:uri ,(eglot--path-to-uri fname)))
+			   :success-fn
+			   (lambda (result)
+			     (let ((status (plist-get result :status)))
+			       (if (eq status 0)
+				   (progn
+				     (princ (format "Compilation successful! Reverting buffer for %s." docname))
+				     (TeX-revert-document-buffer docname))
+				   (princ (format "Compilation failed with status %s." status))))))))
+
 (defun tviti/setup-latex-lsp ()
   (when (eq tviti/linter 'flymake)
     ;; Retain the default latex flymake linter
