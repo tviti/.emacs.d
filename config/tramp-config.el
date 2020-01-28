6c4d2cba0c897f031ff403a9b3b67622fa0b542d
Taylor Viti <tviti@hawaii.edu>
2020-01-27 15:07:47 -1000

    Added xcati tramp-method for starting an interactive sess. on UHHPC.

--- config/tramp-config.el
+++ config/tramp-config.el
@@ -5,4 +5,22 @@
 
 (setq tramp-auto-save-directory "~/.emacs.d/tramp-autosave")
 
+;; Interactive connection to UHHPC's xCAT cluster. This should be called as a
+;; multi-hop connection (e.g. "C-x C-f <user>@<login-node>|xcati:self@<partition>")
+(add-to-list 'tramp-methods
+	     '("xcati"
+	       (tramp-login-program "srun")
+	       (tramp-login-args
+		(("-I")
+		 ("-p" "%h") ; Partition to request
+		 ("-c" "1") ; Num. cpus to request
+		 ("--mem=6G")
+		 ("-t" "60") ; Timeout
+		 ("--pty" "/bin/sh")))
+	       (tramp-login-env (("SHELL") ("/bin/sh")))
+	       (tramp-remote-shell "/bin/sh")
+	       (tramp-remote-shell-login
+		("-l"))
+	       (tramp-remote-shell-args ("-c"))))
+
 (provide 'tramp-config)
