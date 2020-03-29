426641e405f46d2d3e9986361b553b0120e0c431
Taylor Viti <tviti@hawaii.edu>
2020-03-28 14:54:34 -1000

    init: Removed explicit package disabling.

--- init.el
+++ init.el
@@ -6,9 +6,6 @@
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
-;; Enable/disable specific packages
-(setq package-load-list '(all (jupyter nil)))
-
 ;; Added by Package.el.  This must come before configurations of installed
 ;; packages.  Don't delete this line.  If you don't want it, just comment it out
 ;; by adding a semicolon to the start of the line.  You may delete these
