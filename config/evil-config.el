2e2b2df3366602b6dceee30541d405b84585cd22
Taylor Viti <tviti@hawaii.edu>
2020-05-29 14:05:53 -1000

    evil-config: Started experimenting w/ leader bindings.

--- config/evil-config.el
+++ config/evil-config.el
@@ -58,4 +58,52 @@
 (require 'flyspell-correct-ivy)
 (define-key evil-normal-state-map [remap ispell-word] #'flyspell-correct-wrapper)
 
+;;
+;; Leader-key bindings
+;;
+(evil-set-leader '(normal visual motion) (kbd "SPC"))
+
+;; SPC as leader will be shadowed by certain mode-maps. Clear these bindings.
+(with-eval-after-load 'magit-mode
+  (define-key magit-mode-map (kbd "SPC") nil))
+
+;; dired-mode-map is already being modified by evil-mode, so we use
+;; evil-define-key instead.
+(evil-define-key 'normal dired-mode-map (kbd "SPC") nil)
+
+(with-eval-after-load 'pdf-view
+  (evil-define-key 'normal pdf-view-mode-map (kbd "SPC") nil))
+  
+
+(evil-define-key '(normal visual motion) 'global
+  (kbd "<leader>h") #'evil-window-left
+  (kbd "<leader>j") #'evil-window-down
+  (kbd "<leader>k") #'evil-window-up
+  (kbd "<leader>l") #'evil-window-right
+
+  (kbd "<leader>f") #'find-file
+  (kbd "<leader>s") #'save-buffer
+  (kbd "<leader>b") #'switch-to-buffer
+
+  (kbd "<leader>0") #'delete-window)
+
+(with-eval-after-load 'counsel
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "<leader>rb") #'counsel-bookmark))
+
+(when (>= emacs-major-version 27)
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "<leader>t RET") #'tab-bar-select-tab-by-name
+    (kbd "<leader>t 2") #'tab-bar-new-tab
+    (kbd "<leader>t r") #'tab-bar-rename-tab))
+
+(with-eval-after-load 'magit-mode
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "<leader>ms") #'magit-status
+    (kbd "<leader>ml") #'magit-log-buffer-file))
+
+(with-eval-after-load 'projectile
+  (evil-define-key '(normal visual motion) 'global
+    (kbd "<leader>p") #'projectile-command-map))
+
 (provide 'evil-config)
