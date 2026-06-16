8c89bef476ac293e12f9354296daab5d7324cff0
Taylor Viti <sinkoman@gmail.com>
2026-06-16 14:27:19 -0700

    Set light theme to kaolin-light

--- config/user-globals.el
+++ config/user-globals.el
@@ -11,7 +11,7 @@
   "Key to use as `evil-mode''s leader-key.")
 
 (defvar tviti/theme 'nord)
-(defvar tviti/light-theme 'nord)
+(defvar tviti/light-theme 'kaolin-light)
 (defvar tviti/dark-theme 'nord)
 
 (provide 'user-globals)
